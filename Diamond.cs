c0bcefe6769c7834b752c00bc88206e571aa3618
ShipuD <shipu4@gmail.com>
2022-09-12 13:31:58 +0100

    TDD approach

---
+++ Diamond.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiamondKata
+{
+    public class Diamond
+    {
+        private const char _firstAlphabetChar = 'A';
+        private const string InvalidCharMessage = "Invalid character";
+
+        public Diamond()
+        { 
+        }
+
+        public List<string>  Generate(char maxChar)
+        {
+            IsValidAlphabet(maxChar);
+
+            var maxDiamondRows = (maxChar - _firstAlphabetChar + 1) * 2 - 1;
+
+            List<string> _rows = new List<string>();
+            //Half of diamond
+            for (char currentChar = _firstAlphabetChar; currentChar <= maxChar; currentChar++)
+            {
+                string row = GenerateRow(currentChar, maxChar, maxDiamondRows);
+                _rows.Add(row);
+            }
+            List<string> _bottomRows = Enumerable.Reverse(_rows).ToList();
+            
+            _rows.AddRange(_bottomRows.Skip(1));
+            
+            return _rows;
+        }
+
+        private bool IsValidAlphabet(char v)
+        {
+            if (!Char.IsLetter(v))
+                throw new Exception(InvalidCharMessage);
+
+            return true;
+        }
+
+        private string GenerateRow(char currentChar, char maxChar, int maxDiamondRows)
+        {
+            var externalPadding = maxChar - currentChar;
+            var sb = new StringBuilder();
+
+            sb.Append(new string(' ', externalPadding));
+            
+            sb.Append(currentChar);
+
+            if (currentChar != _firstAlphabetChar)
+            {              
+                var internalPadding = maxDiamondRows - externalPadding * 2 - 2;
+                sb.Append(new string(' ', internalPadding));
+                sb.Append(currentChar);
+            }
+            
+            sb.Append(new string(' ', externalPadding));
+            return sb.ToString();
+        }          
+    }
+}
---
+++ DiamondTest.cs
@@ -0,0 +1,105 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DiamondKata;
+using System;
+using System.Linq;
+
+namespace DiamondKataTest
+{
+    [TestClass]
+    public class DiamondTest
+    {
+        private readonly Diamond _diamond;
+        private const string InvalidCharMessage = "Invalid character"; //Should be in const file
+
+        public DiamondTest()
+        {
+            _diamond = new Diamond();
+        }
+
+        [TestMethod]
+        public void NonEmpty()
+        {
+           List<string> lst = _diamond.Generate('C');
+           Assert.IsTrue(lst.Count > 0);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionForCharStar()
+        {
+            try
+            {
+                _diamond.Generate('*');
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual<string>(InvalidCharMessage, e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void DiamondShouldContainFirstCharForInputA()
+        {
+            List<string> lst = _diamond.Generate('A');
+            //Console.WriteLine(lst[0]);
+            Assert.IsTrue(lst.Count ==1 && lst[0] == "A");
+        }
+        [TestMethod]
+        public void LastRowShouldAlwaysBeA()
+        {
+            List<string> lst = _diamond.Generate('D');
+            //Console.WriteLine(lst[0]);
+            Assert.IsTrue(lst.Count > 0 && lst[lst.Count -1].Trim() == "A");
+        }
+
+        [TestMethod]
+        public void LastRowShouldAlwaysBeSameAsFirstWithSpaces()
+        {
+            List<string> lst = _diamond.Generate('D');            
+            Assert.IsTrue(lst.Count > 0 && lst[lst.Count - 1] == lst[0]);
+        }
+
+        [TestMethod]
+        public void AShouldBeIntheMiddleOfFirstRow()
+        {   
+            var topRow = _diamond.Generate('D').ToArray()[0];
+            var leftHalf = topRow.Substring(0, topRow.Length / 2);
+            var righttHalf = topRow.Substring((topRow.Length / 2) + 1);
+
+            Assert.IsTrue(leftHalf.Reverse().SequenceEqual(righttHalf));
+        }
+        [TestMethod]
+        public void ShouldHaveSameWidthAndHeight()
+        {
+            var diamondInList = _diamond.Generate('E').ToList();
+            Assert.IsTrue(diamondInList.All(r => r.Length == diamondInList.Count));
+        }
+
+        [TestMethod]
+        public void ShouldBeHorizontallySymmetric()
+        {
+            var diamondInArray = _diamond.Generate('D').ToArray();
+            var half = diamondInArray.Length / 2;
+            var topHalf = diamondInArray[..half];
+            var bottomHalf = diamondInArray[(half + 1)..];
+
+            Assert.IsTrue(topHalf.Reverse().SequenceEqual(bottomHalf));
+        }
+
+        [TestMethod]
+        public void ShouldBeVerticallySymmetric()
+        {
+            Assert.IsTrue(_diamond.Generate('D').ToArray()
+                        .All(r =>
+                        {
+                            var half = r.Length / 2;
+                            var leftRowOfHalf = r[..half];
+                            var rightHalfOfRow = r[(half + 1)..];
+                            return leftRowOfHalf.Reverse().SequenceEqual(rightHalfOfRow);
+                        })
+                        );
+        }
+
+
+    }
+}
